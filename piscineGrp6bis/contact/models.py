51080c51155dde594983c2ad4b94da3e13b54872
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2023-12-01 15:58:12 +0100

    models de contact

--- piscineGrp6bis/contact/models.py
+++ piscineGrp6bis/contact/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    nom = models.CharField(max_length=20)
+    adresse_email = models.EmailField(max_length=20)
+    message = models.TextField(null=True)
+    def str(self):
+        return self.nom
