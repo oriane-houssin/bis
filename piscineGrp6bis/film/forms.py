92feffd99863af5af6a7c0e09f7f6dc158507786
oriane-houssin <117643200+oriane-houssin@users.noreply.github.com>
2023-12-01 10:16:18 +0100

    forms

--- piscineGrp6bis/film/forms.py
+++ piscineGrp6bis/film/forms.py
@@ -2,13 +2,13 @@ from django import forms
 from .models import Movie, Director
 
 class MovieForm(forms.Form):
-    title = forms.CharField(max_length=90)
-    director = forms.CharField(max_length=90)
+    title = forms.CharField(max_length=90, error_messages={'required': 'Le titre est obligatoire.'})
+    director = forms.CharField(max_length=90, error_messages={'required': 'Le directeur est obligatoire.'})
     summary = forms.Textarea()
     duration = forms.DateField()
     date = forms.DateField()
 
 class DirectorForm(forms.Form):
-    name = forms.CharField(max_length=90)
-    surname = forms.CharField(max_length=90)
+    name = forms.CharField(max_length=90, error_messages={'required': 'Le prénom est obligatoire.'})
+    surname = forms.CharField(max_length=90, error_messages={'required': 'Le nom est obligatoire.'})
     resume = forms.Textarea()
