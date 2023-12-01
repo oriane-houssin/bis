483d6d7a222c8973aa96c69eaefc6a759478b845
MateoMBR <m.burnichon.pro@gmail.com>
2023-12-01 11:16:54 +0100

    Matéo auth

--- piscineGrp6bis/piscineGrp6bis/urls.py
+++ piscineGrp6bis/piscineGrp6bis/urls.py
@@ -15,9 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+<<<<<<< Updated upstream
 from django.urls import path, include
+=======
+from django.contrib.auth import views as auth_views
+from django.urls import path , include
+>>>>>>> Stashed changes
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('film/', include('film.urls')),
+]
+
+urlpatterns = [
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
